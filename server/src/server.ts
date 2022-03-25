39a071b4d3e59892b8555d190c1f397d922c6409
Angus Finch <developer.finchie@gmail.com>
2022-03-25 18:49:48 +1100

    Implement basic data validation
    This adds simple data validation for the weather data generation, making sure that only valid data can be accessed.
    
    Next this validated data will need to be sent to the client over UDP.

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,81 @@ import { generate_weather_data } from '../../emulator/src/weather_output';
 const HOSTNAME = 'localhost';
 const API_PORT = 8000;
 
+// Generic interface for valid weather data
+// All of these types are optional to include case where sensor reading is invalid
+interface WeatherData {
+  ambient_temp?: number,
+  track_temp?: number,
+  humidity?: number,
+  precipitation?: number,
+  wind_speed?: number,
+  wind_dir?: number,
+}
+
+function validate_within_range(min: number, max: number, value: number | null | Function) {
+  if (typeof value === "function" || value == null || !value) {
+    console.log(`Discarding value due to bad type! Got: ${value}`);
+    return false;
+  }
+
+  if (min <= value && value <= max) {
+    return true;
+  } else {
+    console.log(`Discarding value due to out-of-range! Got: ${value}`);
+    return false;
+  }
+}
+
+// ------------------------ Data validation ------------------------
+function get_weather_data() {
+  let generated_data: Object = generate_weather_data();
+  let weather_data: WeatherData = {};
+
+  // Validate the known items and nullify everything else
+  // This could be made a lot smarter by analyzing previous values
+  for (const key in generated_data) {
+    const sensor_reading = generated_data[key as keyof Object];
+    let is_valid: boolean = false;
+
+    switch(key) {
+      case "ambient_temp": {
+        is_valid = validate_within_range(0, 50, sensor_reading);
+        break;
+      }
+      case "track_temp": {
+        is_valid = validate_within_range(0, 100, sensor_reading);
+        break;
+      }
+      case "humidity": {
+        is_valid = validate_within_range(0, 1, sensor_reading);
+        break;
+      }
+      case "precipitation": {
+        is_valid = validate_within_range(0, 200, sensor_reading);
+        break;
+      }
+      case "wind_speed": {
+        is_valid = validate_within_range(0, 100, sensor_reading);
+        break;
+      }
+      case "wind_dir": {
+        is_valid = validate_within_range(0, 360, sensor_reading);
+        break;
+      }
+      default: {
+        throw new RangeError(`Unknown weather reading! Got ${key}`);
+      }
+    }
+
+    if (is_valid && typeof sensor_reading === 'number') {
+      const valid_reading: number = sensor_reading;
+      weather_data[key as keyof WeatherData] = valid_reading;
+    }
+  }
+
+  return weather_data;
+}
+
 // ----------------- Set up the express API server -----------------
 const api = express();
 
@@ -47,7 +122,7 @@ const data = Buffer.from('Some Data');
 
 async function sendData(): Promise<void> {
   socket.send(data, 0, data.length, 5000, 'localhost', (err) => {});
-  console.log("Sent data");
+  get_weather_data();
 }
 
 socket.bind(4500);
